b36a76af865df04861c6de36af2e790801638e69
Matthew Mulenga <Matt@Matthew-Mulengas-MacBook-Pro.local>
2016-11-29 15:19:20 -0600

    Added some testing code for listeners.

--- CMPT 270/Assignments/Assignment 5/FlightApp/src/startup/mainWindow.java
+++ CMPT 270/Assignments/Assignment 5/FlightApp/src/startup/mainWindow.java
@@ -5,25 +5,36 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class mainWindow {
 	
+	
 	public static void main(String args[]) {
+		class ClickListener implements ActionListener {
+			public void actionPerformed(ActionEvent event) {
+				System.out.println("Click.");
+			}
+		}
+		
 		JFrame frame = new JFrame();
 		
 		JButton button = new JButton("Click me!");
 		JLabel label = new JLabel("Hello World!");
 		JPanel panel = new JPanel();
 		
+		ActionListener listener = new ClickListener();
+		button.addActionListener(listener);
+		
 		panel.add(button);
 		panel.add(label);
-		//frame.add(panel);
+		frame.add(panel);
 		
 		frame.setSize(400, 400);
 		frame.setTitle("Hello World");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		System.out.println("Heya!");
 		
 		//System.exit(0);
 	}
