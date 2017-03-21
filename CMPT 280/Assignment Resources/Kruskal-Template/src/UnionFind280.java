f4c3f8659986de02d8a8b1e54d7159098f0bd0eb
Matthew Mulenga <Matt@Matthew-Mulengas-MacBook-Pro.local>
2017-03-21 13:12:16 -0600

    Completed the UnionFind280 class.

--- CMPT 280/Assignment Resources/Kruskal-Template/src/UnionFind280.java
+++ CMPT 280/Assignment Resources/Kruskal-Template/src/UnionFind280.java
@@ -3,6 +3,7 @@
 import lib280.graph.Edge280;
 import lib280.graph.GraphAdjListRep280;
 import lib280.graph.Vertex280;
+import lib280.graph.WeightedGraphAdjListRep280;
 
 
 public class UnionFind280 {
@@ -26,8 +27,18 @@ public class UnionFind280 {
 	 */
 	public int find(int id) {
 		// TODO - Write this method
+		int x = id;
+
+		// Go to the first edge of the designated vertex.
+		G.eGoFirst(G.vertex(x));
+
+		// Check if the edge exists and if it has an outgoing edge to the next vertex
+		while(G.eItemExists() && G.isAdjacent(x, G.eItemAdjacentIndex())) {
+			x = G.eItemAdjacentIndex();
+			G.eGoFirst(G.vertex(x));
+		}
 		
-		return 0;  // Remove this when you are ready.  It is just a placeholder to prevent a compiler error.
+		return x;
 	}
 	
 	/**
@@ -37,9 +48,42 @@ public class UnionFind280 {
 	 */
 	public void union(int id1, int id2) {
 		// TODO - Write this method.
-		
+
+		if(find(id1) == find(id2)) {
+			return;
+		}
+
+		G.addEdge(id1, id2);
 	}
-	
+
+	public static void main(String args[]) {
+		UnionFind280 U = new UnionFind280(8);
+
+		U.union(1, 2);
+
+		if(U.find(1) !=  2) {
+			System.out.println("Error: Something went wrong in the union() method.");
+		}
+
+		U.union(3, 2);
+		U.union(7, 3);
+		U.union(4, 6);
+		U.union(6, 5);
+
+		if(U.find(7) !=  2) {
+			System.out.println("Error: Something went wrong in the union() method.");
+		}
+
+		U.union(5, 2);
+
+		// Using the same subsets found in the assignment outline, test the find() method.
+		for(int i = 1; i <= 7; i++) {
+			if(U.find(i) != 2) {
+				System.out.println("Error: Something went wrong in the find() method.");
+			}
+		}
+
+	}
 	
 	
 }
