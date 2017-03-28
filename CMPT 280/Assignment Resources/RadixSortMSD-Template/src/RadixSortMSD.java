17ef83d3f900c911d3bb0f20c86fe3d20c02f7c0
Matthew Mulenga <Matt@Matthew-Mulengas-MacBook-Pro.local>
2017-03-28 02:53:40 -0600

    Started Assignment 8.

--- CMPT 280/Assignment Resources/RadixSortMSD-Template/src/RadixSortMSD.java
+++ CMPT 280/Assignment Resources/RadixSortMSD-Template/src/RadixSortMSD.java
@@ -1,19 +1,59 @@
+import lib280.list.LinkedList280;
+import sun.awt.image.ImageWatched;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
-public class RadixSortMSD {
+public class RadixSortMSD<I> {
 
 	// *************************************************************
 	// TODO Write YOUR RADIX SORT HERE
 	// *************************************************************
+
+	/**
+	 *
+	 * @param keys
+	 * @param R
+	 */
+	public void MSDRadixSort(I[] keys, int R) {
+		sortByDigit(keys, R, 0);
+	}
+
+	/**
+	 *
+	 * @param keys
+	 * @param R
+	 * @param i
+	 */
+	public void sortByDigit(I[] keys, int R, int i) {
+		LinkedList280<I>[] list = new LinkedList280[R];
+
+		for(int k = 0; k < R - 1; k++) {
+			list[k] = new LinkedList280<I>();
+		}
+
+		for(int k = 0; k < keys.length; k++) {
+			if(keys[k].charAt(i) == k) {
+				list[k].insert(keys[k]);
+			}
+		}
+
+		for(int k = 0; k < R - 1; k++) {
+			if(k < keys[k].length()) {
+				MSDRadixSort(list[k], i + 1);
+			}
+		}
+	}
 	
+
 	public static void main(String args[]) {
 		
 		// *************************************************************
 		// Change the input file by changing the line below.
 		// *************************************************************
-		String inputFile = "RadixSortMSD-Template/words-basictest.txt";
+		String inputFile = "words-basictest.txt";
 		
 		// Initialize a scanner to read the input file.
 		Scanner S = null;
@@ -53,11 +93,11 @@ public class RadixSortMSD {
 		// Uncomment this section if you want the sorted list to be printed to the console.
 		// (Good idea for testing with words-basictest.txt; leave it commented out though
 		// for testing files with more than 50 words).
-		/*
+
 		for(int i=0; i < items.length; i++) {
 			System.out.println(items[i]);
 		}
-		*/
+
 		
 		// Print out how long the sort took in milliseconds.
 		System.out.println("Sorted " + items.length + " strings in " + (stopTime-startTime)/1000000.0 + "ms");
