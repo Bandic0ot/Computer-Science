eb358106ac5c1ea1532904b274d7f8458eab9b48
mmulenga <matt.mulengawoo@gmail.com>
2017-10-26 02:20:04 -0600

    Added player, zombie, and bullet movement.

---
+++ CMPT 306/Assignment 2/Top Down Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿// Matthew Mulenga
+// mam558
+// 11144528
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator anim;
+
+    public GameObject gun;
+    public GameObject bullet;
+
+
+    public float movement_speed = 3.0f;
+    public float rotation_speed = 150.0f;
+    public float bullet_speed = 10.0f;
+
+    float bullet_cooldown = 1.0f;
+    float cur_bullet_cooldown = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //-------- Movement Mechanics --------
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        this.transform.Rotate(new Vector3(0, 0, -xInput * rotation_speed * Time.deltaTime));
+        this.transform.Translate(Vector2.right * yInput * movement_speed * Time.deltaTime);
+
+        anim.SetBool("Moving", yInput != 0);
+
+        //-------- Shooting Mechanics --------
+        bool is_shooting = Input.GetButton("Jump");
+        cur_bullet_cooldown -= Time.deltaTime;
+
+        if (is_shooting && cur_bullet_cooldown < Time.time) {
+            // Match the rotation and orientation of the projectile to the gun.
+            // Make the projectile spawn at the origin of the gun, not the player body.
+            GameObject newly_created_bullet = Instantiate(bullet, gun.transform.position, gun.transform.rotation * Quaternion.Euler(0, 0, -225.00f));
+
+            // Set its velocity
+            Rigidbody2D new_bullet_physics = newly_created_bullet.GetComponent<Rigidbody2D>();
+            new_bullet_physics.velocity = this.transform.right * bullet_speed;
+
+            // Record when we shot the gun.
+            cur_bullet_cooldown = Time.time + bullet_cooldown;
+        }
+	}
+
+    public void die()
+    {
+        Destroy(this.gameObject);
+
+        // Reset the game if the player dies
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ CMPT 306/Assignment 2/Top Down Shooter/Assets/Scripts/Zombie.cs
@@ -0,0 +1,49 @@
+﻿// Matthew Mulenga
+// mam558
+// 11144528
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+
+    GameObject target;
+
+    public float movement_speed = 2.0f;
+    public float damage = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.transform.position, movement_speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.SendMessage("die");
+        }
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            die();
+        }
+    }
+
+    public void die()
+    {
+        Destroy(this.gameObject);
+    }
+}
