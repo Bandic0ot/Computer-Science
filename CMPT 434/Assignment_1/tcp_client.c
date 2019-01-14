ac5aba99720315d4cdfd364e637ac0f955497e75
mmulenga <matt.mulengawoo@gmail.com>
2019-01-14 00:14:10 -0600

    Implemented input parsing for client.

--- CMPT 434/Assignment_1/tcp_client.c
+++ CMPT 434/Assignment_1/tcp_client.c
@@ -1,50 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <string.h>
 
-void get(char* local_file, char* remote_file) {
+void get(char *local_file, char *remote_file) {
   printf("GET: %s %s\n", local_file, remote_file);
 }
 
-void put(char* local_file, char* remote_file) {
+void put(char *local_file, char *remote_file) {
   printf("PUT: %s %s\n", local_file, remote_file);
 }
 
+// Takes the given input and separates it by spaces, 
+// each word becomes an element of the provided array.
+void parse_input(char *input, char **input_args) {
+    char *token;
+    int counter = 0;
+
+    token = strtok(input, " ");
+    input_args[counter] = token;
+
+    while(token != NULL) {
+      counter++;
+      token = strtok(NULL, " ");
+      input_args[counter] = token;
+    }
+
+    if(counter == 1) {
+      if(strcmp(strtok(input_args[0], "\n"), "quit") == 0) {
+        exit(0);
+      }
+    } else if(counter == 3) {
+      if(strcmp(input_args[0], "get") == 0) {
+        get(input_args[1], input_args[2]);
+        return;
+      } else if(strcmp(input_args[0], "put") == 0) {
+        put(input_args[1], input_args[2]);
+        return;
+      }
+    }
+    
+    // If the input arguments aren't valid the default behaviour is 
+    // to reset the arguments to NULL and return an invalid message.
+    input = NULL;
+    input_args = NULL;
+
+    printf("Invalid command.\n");
+}
+
 int main(int argc, char *argv[]) {
+  int BUFFER = 100;
   // Validate the given command line argument.
   if(argc == 2) {
     printf("%s\n", argv[argc - 1]);
   } else {
-    printf("Invalid arguments.\n");
+    printf("Invalid command line arguments.\n");
     return 1;
   }
 
   // Program loop
   while(1) {
+    char *input = malloc(sizeof(char) * BUFFER);
+    char **input_args = malloc(sizeof(char *) * (BUFFER / 2));
+
     printf("Please enter a command [get|put|quit] [local] [remote]:\n");
+    fgets(input, BUFFER, stdin);
 
-    char command[5];
-    char local_file[50];
-    char remote_file[50];
-
-    // Check to see if the right number of strings was given.
-    if(scanf("%s %s %s", command, local_file, remote_file) <= 0) {
-      printf("Invalid command.\n");
-      continue;
-    } else {
-      // Match the user input to a known command.
-      if(strcmp(command, "get") == 0) {
-        get(local_file, remote_file);
-      } else if(strcmp(command, "put") == 0) {
-        put(local_file, remote_file);
-      } else if(strcmp(command, "quit") == 0) {
-        return 0;
-      } else {
-        printf("Invalid command.\n");
-        continue;
-      }
-    }
+    parse_input(input, input_args);
   }
 
   return 0;
