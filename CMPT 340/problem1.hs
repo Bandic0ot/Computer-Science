a7680378f83f02e6cc05ec4fd100cfe81b1d0964
Matthew Mulenga <mam558@minion1.usask.ca>
2019-02-28 17:51:57 -0600

    Home time.

--- CMPT 340/problem1.hs
+++ CMPT 340/problem1.hs
@@ -1,8 +1,8 @@
-unfold :: (a -> Bool) -> (a -> b) -> (a -> a) -> a -> [b]
+--unfold :: (a -> Bool) -> (a -> b) -> (a -> a) -> a -> [b]
 unfold p h t x 
   | p x        = []
   | otherwise  = h x : unfold p h t (t x)
 
-foldmap f = unfold a f
-  where
-    a = (==[])
+--mapf :: ([a] -> a) -> [a] -> [a]
+--mapf f (x:xs) = unfold (== []) f x
+
